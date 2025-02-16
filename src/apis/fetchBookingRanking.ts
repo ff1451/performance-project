8fdbb0f26f60b080c76faf4a58e4c4a514a1f772
ff1451 <ff1451@gmail.com>
2025-02-16 21:09:26 +0900

    refactor: 불필요한 로직 제거

--- src/apis/fetchBookingRanking.ts
+++ src/apis/fetchBookingRanking.ts
@@ -22,33 +22,18 @@ export async function fetchBookingRanking(): Promise<boxoffice[]> {
 
     const rankings = data.boxof;
 
-    return Array.isArray(rankings)
-      ? rankings.map((item: any) => ({
-          ranking: Number(item.rnum) || 0,
-          name: item.prfnm || "",
-          period: item.prfpd || "",
-          place: item.prfplcnm || "",
-          seatCount: Number(item.seatcnt) || 0,
-          playCount: Number(item.prfdtcnt) || 0,
-          area: item.area || "",
-          poster: item.poster || "",
-          id: item.mt20id || "",
-          genre: item.cate || "",
-        }))
-      : [
-          {
-            ranking: Number(rankings.rnum) || 0,
-            name: rankings.prfnm || "",
-            period: rankings.prfpd || "",
-            place: rankings.prfplcnm || "",
-            seatCount: Number(rankings.seatcnt) || 0,
-            playCount: Number(rankings.prfdtcnt) || 0,
-            area: rankings.area || "",
-            poster: rankings.poster || "",
-            id: rankings.mt20id || "",
-            genre: rankings.cate || "",
-          },
-        ];
+    return rankings.map((item: any) => ({
+      ranking: Number(item.rnum) || 0,
+      name: item.prfnm || "",
+      period: item.prfpd || "",
+      place: item.prfplcnm || "",
+      seatCount: Number(item.seatcnt) || 0,
+      playCount: Number(item.prfdtcnt) || 0,
+      area: item.area || "",
+      poster: item.poster || "",
+      id: item.mt20id || "",
+      genre: item.cate || "",
+    }));
   } catch (error) {
     console.error("예매 순위를 가져오는 중 오류 발생:", error);
     return [];
