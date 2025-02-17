28be1ebf90d9e531f74a7b9c84e3e862200918a8
ff1451 <ff1451@gmail.com>
2025-02-17 11:59:08 +0900

    refactor : useMemo 이용 성능 최적화

--- src/components/Performance/CategoryNav/PerformanceCategoryNav.tsx
+++ src/components/Performance/CategoryNav/PerformanceCategoryNav.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import styles from "./PerformanceCategoryNav.module.css";
 import { categories } from "@/constants/category";
 
@@ -10,30 +11,30 @@ export default function PerformanceCategoryNav({
   selectedCategory,
   setSelectedCategory,
 }: PerformanceCategoryNavProps) {
-  return (
-    <ul className={styles["performance__genre-nav"]}>
-      {categories.map((category, index) => (
-        <li
-          key={category.category}
-          className={`${styles["performance__genre-item"]} ${
-            index === categories.length - 1
-              ? styles["performance__genre-item--last"]
-              : ""
-          } ${
-            selectedCategory === category.category
-              ? styles["performance__genre-item--active"]
-              : ""
-          }`}
-          onClick={() => setSelectedCategory(category.category)}
-        >
-          {category.category}
-          {category.subCategory && (
-            <p className={styles["performance__genre-text"]}>
-              ({category.subCategory})
-            </p>
-          )}
-        </li>
-      ))}
-    </ul>
-  );
+  const categoryList = useMemo(() => {
+    return categories.map((category, index) => (
+      <li
+        key={category.category}
+        className={`${styles["performance__genre-item"]} ${
+          index === categories.length - 1
+            ? styles["performance__genre-item--last"]
+            : ""
+        } ${
+          selectedCategory === category.category
+            ? styles["performance__genre-item--active"]
+            : ""
+        }`}
+        onClick={() => setSelectedCategory(category.category)}
+      >
+        {category.category}
+        {category.subCategory && (
+          <p className={styles["performance__genre-text"]}>
+            ({category.subCategory})
+          </p>
+        )}
+      </li>
+    ));
+  }, [selectedCategory]);
+
+  return <ul className={styles["performance__genre-nav"]}>{categoryList}</ul>;
 }
